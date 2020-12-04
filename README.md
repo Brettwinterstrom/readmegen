739fa6594535f47bd43070abb4c7f8f82c5894c3
Brett Winterstrom <brettwinterstrom@yahoo.com>
2020-12-03 16:28:56 -0800

    fixed table of contents working on finalizing read me look

--- README.md
+++ README.md
@@ -1,32 +1,46 @@
 
 ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
-# read me generator
-**Version 1.0**
+# 
+**Version **
+
 ## Description
-### this is a description thathlktakljtkltjalktjlkatj
-## Table of Contents
-- User Story
-- Installation
-- Usage
-- Liscense
-- Contributions
-- Questions
-## User Story
-i want a readme generated
-## Installation Command
+### 
+
+### Table of Contents
+- [UserStory](#UserStory)
+- [Installation](#Installation)
+- [Test](#Test)
+- [License](#License)
+- [Contributions](#contributions)
+- [Questions](#Questions)
+
+
+<a name="userstory"></a>
+## UserStory
+undefined
+---
+<a name="Installation"></a>
+## Installation
 To install necessary dependencies, run the following command:
-npm i
-## Test Commands
-npm run test
-## LICENSE & COPYRIGHT
+> 
+---
+<a name="Test"></a>
+## Test
+> 
+---
+<a name="License"></a>
+## License
 This project is licensed under the MIT license
+---
+<a name="contributions"></a>
+## Contributions
 
 
-
-Git Hub User Name: brettwinterstrom
-
+---
+<a name="Questions"></a>
+## Questions
 For more information or questions 
-Email me @ brettwinterstrom@yaho
---
-### Example of what the end result should look like 
+Email me @ 
+Git Hub User Name: 
+---
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,33 +4,47 @@ function generateMarkdown(data) {
 ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
 # ${data.title}
 **Version ${data.version}**
+
 ## Description
 ### ${data.description}
-## Table of Contents
-- User Story
-- Installation
-- Usage
-- Liscense
-- Contributions
-- Questions
-## User Story
-${data.userStory}
-## Installation Command
+
+### Table of Contents
+- [UserStory](#UserStory)
+- [Installation](#Installation)
+- [Test](#Test)
+- [License](#License)
+- [Contributions](#contributions)
+- [Questions](#Questions)
+
+
+<a name="userstory"></a>
+## UserStory
+${data.UserStory}
+---
+<a name="Installation"></a>
+## Installation
 To install necessary dependencies, run the following command:
-${data.installation}
-## Test Commands
-${data.test}
-## LICENSE & COPYRIGHT
+> ${data.installation}
+---
+<a name="Test"></a>
+## Test
+> ${data.test}
+---
+<a name="License"></a>
+## License
 This project is licensed under the ${data.license} license
+---
+<a name="contributions"></a>
+## Contributions
+${data.contributor}
 
-
-
-Git Hub User Name: ${data.github}
-
+---
+<a name="Questions"></a>
+## Questions
 For more information or questions 
 Email me @ ${data.email}
---
-### Example of what the end result should look like 
+Git Hub User Name: ${data.github}
+---
   `;
 }
 
